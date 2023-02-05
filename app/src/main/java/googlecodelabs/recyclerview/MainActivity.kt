89a7e8fdf03886091480b69c7753955b53e52d92
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2023-02-05 17:38:35 +0900

    3. Task 1: Create a new project and dataset
    1.2. Add code to create data

--- app/src/main/java/googlecodelabs/recyclerview/MainActivity.kt
+++ app/src/main/java/googlecodelabs/recyclerview/MainActivity.kt
@@ -2,10 +2,17 @@ package googlecodelabs.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.LinkedList
 
 class MainActivity : AppCompatActivity() {
+    private val mWordList : LinkedList<String> = LinkedList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        for(i in 0 until 20){
+            mWordList.addLast("Word $i")
+        }
     }
 }

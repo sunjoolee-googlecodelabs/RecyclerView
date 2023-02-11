af5412e2a7b405755a65442b8117e11c45bd00c0
sunjoolee <spottypoo124@gmail.com>
2023-02-11 21:02:23 +0900

    3. Task 1: Create a new project and dataset
    1.3. Change the FAB icon

--- app/src/main/java/googlecodelabs/recyclerview/MainActivity.kt
+++ app/src/main/java/googlecodelabs/recyclerview/MainActivity.kt
@@ -2,6 +2,7 @@ package googlecodelabs.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.util.LinkedList
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setImageResource(R.drawable.ic_add_for_fab)
 
         for(i in 0 until 20){
             mWordList.addLast("Word $i")
